effd54dcfe6878e5e27d2c6d7fee592e4b99bdb3
Sanal(Qburst) <sanalv@qburst.com>
2016-01-31 16:31:13 +0530

    Completed Project0 for review

--- Udacity_project0/app/src/main/java/myportfolio/nanodegree/com/udacity_project0/PortfolioActivity.java
+++ Udacity_project0/app/src/main/java/myportfolio/nanodegree/com/udacity_project0/PortfolioActivity.java
@@ -1,15 +1,21 @@
 package myportfolio.nanodegree.com.udacity_project0;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class PortfolioActivity extends AppCompatActivity {
+public class PortfolioActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button spotifyAppButton;
+    private Button scoresAppButton;
+    private Button libraryAppButton;
+    private Button buildItBiggerButton;
+    private Button xyzReaderAppButton;
+    private Button capstoneProjectButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +23,71 @@ public class PortfolioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_portfolio);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initializeViews();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    /**
+     * This method will initialize all views in this activity and configure listners for initializes views.
+     */
+    private void initializeViews() {
+
+        spotifyAppButton = (Button) findViewById(R.id.spotify_button);
+        scoresAppButton = (Button) findViewById(R.id.scores_app);
+        libraryAppButton = (Button) findViewById(R.id.library_app);
+        buildItBiggerButton = (Button) findViewById(R.id.build_it_bigger_button);
+        xyzReaderAppButton = (Button) findViewById(R.id.xyz_reader_app);
+        capstoneProjectButton = (Button) findViewById(R.id.capstone_project);
+
+        setListener();
+    }
+
+    /**
+     * Configure listners for required views
+     */
+    private void setListener() {
+
+        spotifyAppButton.setOnClickListener(this);
+        scoresAppButton.setOnClickListener(this);
+        libraryAppButton.setOnClickListener(this);
+        buildItBiggerButton.setOnClickListener(this);
+        xyzReaderAppButton.setOnClickListener(this);
+        capstoneProjectButton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_portfolio, menu);
-        return true;
-    }
+    public void onClick(View view) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (view.getId()) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.spotify_button:
+                showToast("This button will show my music streaming App");
+                break;
+            case R.id.scores_app:
+                showToast("This button will show my Sports score App");
+                break;
+            case R.id.library_app:
+                showToast("This button will show my library App");
+                break;
+            case R.id.build_it_bigger_button:
+                showToast("This will take you to advanced project topics");
+                break;
+            case R.id.xyz_reader_app:
+                showToast("This will take you to my reader app");
+                break;
+            case R.id.capstone_project:
+                showToast("This will take you to my final project");
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    /***
+     * this method will show toast to user.
+     *
+     * @param message : message to be shown to the user
+     */
+    public void showToast(String message) {
+        Toast.makeText(PortfolioActivity.this, message, Toast.LENGTH_SHORT).show();
+
     }
 }
